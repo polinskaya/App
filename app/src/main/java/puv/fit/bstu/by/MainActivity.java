0d3ca5b0537a55f96e0a52dae9734ee98cf06c8c
Yulia Polinskaya <polinskaya27@mail.ru>
2020-09-02 20:11:06 +0300

    lr1

--- app/src/main/java/puv/fit/bstu/by/MainActivity.java
+++ app/src/main/java/puv/fit/bstu/by/MainActivity.java
@@ -3,6 +3,10 @@ package puv.fit.bstu.by;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import text.TextFunc;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity","conter =" + count);
+        }
+        displayText();
+    }
+
+    private void displayText() {
+        TextFunc tf = new TextFunc();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
